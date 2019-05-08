b099b1940a8e160782e1a4addaf95a6f7036c1b3
Mackenzie Brown <mbrown20@zagmail.gonzaga.edu>
2019-05-08 16:51:03 -0700

    ensemble knn classification

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main():
     print("HI")
     table = utils.read_table('red_wine_quality.csv')
     header = table[0]
-    data = table[1:]
-    utils.knn_random_subsampling(data, [3, 7, 10], 11, 5)
-
-
+    data = table[1:100]
+    #utils.knn_random_subsampling(data, 5, [3, 7, 10], 11, 5)
+    utils.bootstrap_aggregation(data, 1, [1, 2, 3, 4, 5], 11, .50, 5)
+        
 
 if __name__ == "__main__":
     main()
